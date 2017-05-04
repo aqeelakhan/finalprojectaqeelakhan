fa1d306c4f8c43556ac7d895d591e2feb201bea7
aqeelakhan <redda012@newschool.edu>
2017-05-04 17:52:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+console.log("linked")
+
+document.getElementById("demo1").onmouseover = function() {mouseOver1()};
+document.getElementById("demo1").onmouseout = function() {mouseOut1()};
+
+function mouseOver1() {
+    document.getElementById("demo1").style.color = "red";
+}
+
+function mouseOut1() {
+    document.getElementById("demo1").style.color = "darkblue";
+}
+
+document.getElementById("demo2").onmouseover = function() {mouseOver2()};
+document.getElementById("demo2").onmouseout = function() {mouseOut2()};
+
+function mouseOver2() {
+    document.getElementById("demo2").style.color = "red";
+}
+
+function mouseOut2() {
+    document.getElementById("demo2").style.color = "darkblue";
+}
+
+
+
+document.getElementById("facebook").onmouseover = function() {mouseOver3()};
+document.getElementById("facebook").onmouseout = function() {mouseOut3()};
+
+function mouseOver3() {
+    document.getElementById("facebook").style.opacity = "0.7";
+}
+
+function mouseOut3() {
+    document.getElementById("facebook").style.opacity = "1";
+}
+
+
+document.getElementById("twitter").onmouseover = function() {mouseOver4()};
+document.getElementById("twitter").onmouseout = function() {mouseOut4()};
+
+function mouseOver4() {
+    document.getElementById("twitter").style.opacity = "0.7";
+}
+
+function mouseOut4() {
+    document.getElementById("twitter").style.opacity = "1";
+}
+
+
+
+document.getElementById("instagram").onmouseover = function() {mouseOver5()};
+document.getElementById("instagram").onmouseout = function() {mouseOut5()};
+
+function mouseOver5() {
+    document.getElementById("instagram").style.opacity = "0.7";
+}
+
+function mouseOut5() {
+    document.getElementById("instagram").style.opacity = "1";
+}
+
+
+
+document.getElementById("github").onmouseover = function() {mouseOver6()};
+document.getElementById("github").onmouseout = function() {mouseOut6()};
+
+function mouseOver6() {
+    document.getElementById("github").style.opacity = "0.7";
+}
+
+function mouseOut6() {
+    document.getElementById("github").style.opacity = "1";
+}
+
+
+document.getElementById("bebelink").onmouseover = function() {mouseOver7()};
+document.getElementById("bebelink").onmouseout = function() {mouseOut7()};
+
+function mouseOver7() {
+    document.getElementById("bebelink").style.color = "red";
+}
+
+function mouseOut7() {
+    document.getElementById("bebelink").style.color = "darkblue";
+}
